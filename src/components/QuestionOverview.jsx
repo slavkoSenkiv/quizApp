a87bd689409ee8a9b130c60fc928ec4fe971f85f
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-20 07:28:07 +0100

    finish the app

--- src/components/QuestionOverview.jsx
+++ src/components/QuestionOverview.jsx
@@ -1,7 +1,8 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../../questions";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question";
+import Summary from "./Summary";
+
 
 export default function QuestionOverview() {
 const [userAnswers, setUserAnswers] = useState([])
@@ -24,7 +25,7 @@ const [userAnswers, setUserAnswers] = useState([])
 
   if (quizIsComplete) {
     return (
-      <p>summary placeholder</p>
+      <Summary userAnswers={userAnswers}/>
     );
   }
 
