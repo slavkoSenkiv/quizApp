c36d294744da231f28fc4af154de79e327d41403
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-21 18:21:25 +0100

    remove all the things

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import quizLogo  from "../assets/quiz-logo.png";
-export default function Header() {
-  return (
-    <header>
-      <img src={quizLogo} alt="" />
-      <h1>Reactoquiz</h1>
-    </header>
-  );
-}
