ca58180001e5f0e36d9ff836211e327c10c3108e
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-22 06:59:19 +0100

    writing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Quiz from "./components/QuestionOverview";
 
-export default function App () {
+export default function App() {
   return (
     <>
-    <Header />
-    <main>
-      <Quiz />
-    </main>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
     </>
-  )
+  );
 }
