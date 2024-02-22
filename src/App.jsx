c6760568b833a2b597389e017c0aae2de9c67bf5
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-22 07:14:38 +0100

    add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import Header from "./components/Header";
 import Quiz from "./components/QuestionOverview";
 
+/* 
+  header
+  quizOverview
+    summary 
+    OR
+    question
+      countdown
+        progres
+      answers
+        ul>li>btn
+    countdown
+
+*/
+
 export default function App() {
   return (
     <>
